7286412fab8982c7342d427d78588ade216684f5
Olha Vyzhevska <ovizhevska@gmail.com>
2020-04-07 17:36:26 +0200

    New basket page tests

---
+++ pages/basket_page.py
@@ -0,0 +1,9 @@
+from pages.base_page import BasePage
+from pages.locators import BasketPageLocators
+
+class BasketPage(BasePage):
+    def should_not_be_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Basket is not empty"
+
+    def should_be_empty_basket_massege(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET), "Basket is not empty"
